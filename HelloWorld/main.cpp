b0594a412abb475b026bb865c80c157a4e001cfc
Jasmine <32368030+jphrivett@users.noreply.github.com>
2017-12-08 16:48:48 +0100

    cout

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main()
 {
+    int x = 5;
     cout << "Hello world!" << endl;
+    cout << "Number " << x;
+    cout << " is the best" << endl;
+    cout << "New line.";
     return 0;
 }
