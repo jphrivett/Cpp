2ee6da79805a6741f388b1108a04cc092afa2238
Jasmine <32368030+jphrivett@users.noreply.github.com>
2017-12-08 17:02:48 +0100

    cin

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -8,6 +8,13 @@ int main()
     cout << "Hello world!" << endl;
     cout << "Number " << x;
     cout << " is the best" << endl;
-    cout << "New line.";
+    cout << "New line." << endl;
+
+    cout << "Enter number ";
+    int y;
+    cin >> y;
+    cout << "You entered " << y << endl;
+
     return 0;
+
 }
