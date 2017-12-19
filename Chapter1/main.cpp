b9afbb64c5e3abb7182f2c0e6bcbd64d8579dc8c
Jasmine <32368030+jphrivett@users.noreply.github.com>
2017-12-19 21:01:46 +0100

    Changed to uniform initialization

--- Chapter1/main.cpp
+++ Chapter1/main.cpp
@@ -2,10 +2,10 @@
 
 int main()
 {
-    int int1 = readNumber();
-    int int2 = readNumber();
+    int int1{readNumber()};
+    int int2{readNumber()};
 
-    int summedInts = int1 + int2;
+    int summedInts{int1 + int2};
 
     writeAnswer(summedInts);
     return 0;
