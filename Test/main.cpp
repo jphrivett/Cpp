4274c68c4258caa995dbd3f45a8debd42d476876
Jasmine <32368030+jphrivett@users.noreply.github.com>
2018-01-10 21:40:34 +0100

    Infinite loop

---
+++ Test/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    while (true) // infinite loop
+    {
+        std::cout << "Enter 0 to exit or anything else to continue: ";
+        int num;
+        std::cin >> num;
+
+        // exit loop if user enters 0
+        if (num == 0)
+            break;
+    }
+
+    std::cout << "We're out!\n";
+
+    return 0;
+}
