57a86bfd0e2be10fbdb7173afe82bdea77220e7e
Jasmine <32368030+jphrivett@users.noreply.github.com>
2018-01-10 21:44:17 +0100

    Difference between break and return

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 
-int main()
+int breakOrReturn()
 {
     while (true) // infinite loop
     {
-        std::cout << "Enter 0 to exit or anything else to continue: ";
-        int num;
-        std::cin >> num;
+        std::cout << "Enter 'b' to break or 'r' to return: ";
+        char ch;
+        std::cin >> ch;
 
-        // exit loop if user enters 0
-        if (num == 0)
-            break;
+        if (ch == 'b')
+            break; // execution will continue at the first statement beyond the loop
+
+        if (ch == 'r')
+            return 1; // return will cause the function to immediately return to the caller (in this case, main())
     }
 
-    std::cout << "We're out!\n";
+    // breaking the loop causes execution to resume here
+
+    std::cout << "We broke out of the loop\n";
+
+    return 0;
+}
+
+int main()
+{
+    int returnValue = breakOrReturn();
+    std::cout << "Function breakOrContinue returned " << returnValue << '\n';
 
     return 0;
 }
