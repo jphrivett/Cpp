0c0d25b491715dbe5403d204e5f203e1c6052cd9
Jasmine <32368030+jphrivett@users.noreply.github.com>
2018-01-18 11:00:46 +0100

    std::cout prints type char* as string and not as a memory address

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,23 +1,9 @@
 #include <iostream>
 
-// parameter ptr contains a copy of the array's address
-void changeArray(int *ptr)
-{
-    *ptr = 5; // so changing an array element changes the _actual_ array
-    std::cout << sizeof(ptr) << "\n";
-    std::cout << sizeof(*ptr) << "\n";
-}
-
 int main()
 {
-    int array[] = { 1, 1, 2, 3, 5, 8, 13, 21 };
-    std::cout << "Element 0 has value: " << array[0] << '\n';
-    std::cout << sizeof(array) << "\n";
+    char c = 'P';
+    std::cout << &c;
 
-    changeArray(array);
-
-    std::cout << "Element 0 has value: " << array[0] << '\n';
-    std::cout << sizeof(array) << "\n";
-
-     return 0;
+    return 0;
 }
