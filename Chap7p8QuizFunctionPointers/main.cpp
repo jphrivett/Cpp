24136526f5dc6936c054beeb75699e35d7e73285
Jasmine <32368030+jphrivett@users.noreply.github.com>
2018-01-24 16:40:07 +0100

    Finished 1b

--- Chap7p8QuizFunctionPointers/main.cpp
+++ Chap7p8QuizFunctionPointers/main.cpp
@@ -38,6 +38,26 @@ char getChar()
     }
 }
 
+int add(int x, int y)
+{
+    return (x + y);
+}
+
+int subtract(int x, int y)
+{
+    return (x - y);
+}
+
+int multiply(int x, int y)
+{
+    return (x * y);
+}
+
+int divide(int x, int y)
+{
+    return (x / y);
+}
+
 int main()
 {
 
