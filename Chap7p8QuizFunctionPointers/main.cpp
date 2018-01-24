fe835ba7657d13bcbff7ffa5b5be891c87bfbb1e
Jasmine <32368030+jphrivett@users.noreply.github.com>
2018-01-24 18:08:23 +0100

    Finished

--- Chap7p8QuizFunctionPointers/main.cpp
+++ Chap7p8QuizFunctionPointers/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <array>
 
 int getInt()
 {
@@ -58,26 +59,55 @@ int divide(int x, int y)
     return (x / y);
 }
 
+//Put the typedef just before where you need to use it,
+//just like whenever you instantiate anything normally:
+typedef int (*arithmeticFcn)(int, int);
+
+//Put the structure just before where you need to use it,
+//just like whenever you instantiate anything normally:
 struct arithmeticStruct
 {
     char op;
     arithmeticFcn ptr;
 };
 
-//Put the typedef just before where you need to use it,
-//just like whenever you instantiate anything normally:
-typedef int (*arithmeticFcn)(int, int);
+/*static arithmeticStruct arithmeticArray[] {
+	{ '+', add },
+	{ '-', subtract },
+	{ '*', multiply },
+	{ '/', divide }
+};*/
+
+//Two sets of outer braces are required to initialise
+//a std::array of structures, because std::array itself
+//is a structure:
+const static std::array<arithmeticStruct,4> arithmeticArray {{
+	{ '+', add },
+	{ '-', subtract },
+	{ '*', multiply },
+	{ '/', divide }
+}};
 
 arithmeticFcn getArithmeticFunction(char op)
 {
-    switch (op)
+    /*switch (op)
     {
     default:
     case '+': return add;
     case '-': return subtract;
     case '*': return multiply;
     case '/': return divide;
+    }*/
+
+    //Use constant reference to avoid making a copy of
+    //the array and to prevent it from being changed:
+    for (const arithmeticStruct &option : arithmeticArray)
+    {
+        if (option.op == op)
+            return option.ptr;
     }
+
+    return add;
 }
 
 int main()
