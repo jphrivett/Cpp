b49252a180dbd2119e3388dad83ccab38ee01cba
Jasmine <32368030+jphrivett@users.noreply.github.com>
2018-01-26 15:55:32 +0100

    Finished

--- Chap7p11QuizRecursiveFunctions/main.cpp
+++ Chap7p11QuizRecursiveFunctions/main.cpp
@@ -47,7 +47,7 @@ int main()
 }*/
 
 //Q2:
-int sumInts(int x)
+/*int sumInts(int x)
 {
     if (x < 10)
         return x;
@@ -61,8 +61,39 @@ int main()
     std::cout << (0 % 10);
 
     return 0;
+}*/
+
+//Q3:
+void printBinaryDigits(unsigned int n)
+{
+    if (n == 0)
+        return;
+
+    printBinaryDigits(n / 2);
+    std::cout << (n % 2);
 }
 
+void printBinary(int n)
+{
+    if (n == 0)
+        std::cout << '0';
+    else
+        printBinaryDigits(static_cast<unsigned int>(n));
+}
+
+int main()
+{
+    //The bit representation of signed int -15 is exactly
+    //the same as the bit representation of unsigned int
+    //4294967281. Therefore you can simply static cast to
+    //this number and use the function for unsigned ints.
+    //std::cout << static_cast<unsigned int>(n) << '\n';
+    //std::cout << static_cast<int>(static_cast<unsigned int>(n)) << '\n';
+
+    int n = -15;
+
+    printBinary(n);
+}
 
 
 
