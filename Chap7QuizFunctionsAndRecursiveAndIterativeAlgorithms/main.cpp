96f4f4390e7c8b0f95fedf15da76c801e108e4ba
Jasmine <32368030+jphrivett@users.noreply.github.com>
2018-01-26 17:10:53 +0100

    Finished Q1

---
+++ Chap7QuizFunctionsAndRecursiveAndIterativeAlgorithms/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+//Function prototypes:
+//double max(double a, double b);
+//void swap(int &x, int &y);
+//int& getLargestElement(int length, int *array);
+
+int main()
+{
+    return 0;
+}
