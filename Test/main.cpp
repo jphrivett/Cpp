804708b2a6d5f504498e1444004dae409f5d300c
Jasmine <32368030+jphrivett@users.noreply.github.com>
2018-01-26 16:04:55 +0100

    Command line arguements

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
-    char c = 'P';
-    std::cout << &c;
+    std::cout << "There are " << argc << " arguments:\n";
+
+    for (int count = 0; count < argc; ++count)
+        std::cout << count << " " << argv[count] << '\n';
 
     return 0;
 }
