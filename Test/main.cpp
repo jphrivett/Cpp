54ed376605731e3967e4d02944db650890a8b1f3
Jasmine <32368030+jphrivett@users.noreply.github.com>
2018-02-12 17:48:57 +0100

    ID generator using static class members and functions

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,32 +1,23 @@
 #include <iostream>
-#include <string>
-#include <sstream> // for std::stringstream
-#include <cstdlib> // for exit()
 
-class Something
+class idGenerator
 {
-private:
-    int m_value1;
-    double m_value2;
-    char m_value3;
+    static int nextID;
 
 public:
-    Something(int value1, double value2, char value3 = 'c')
-        : m_value1(value1), m_value2(value2), m_value3(value3) // directly initialize our member variables
-    {
-    // No need for assignment here
-    }
-
-    void print()
-    {
-         std::cout << "Something(" << m_value1 << ", " << m_value2 << ", " << m_value3 << ")\n";
-    }
-
+    static int getNextID();
 };
+
+int idGenerator::nextID = 1;
+
+int idGenerator::getNextID()
+{
+    return nextID++;
+}
 
 int main()
 {
-    Something something(1, 2.2); // value1 = 1, value2=2.2, value3 gets default value 'c'
-    something.print();
-    return 0;
+    for (int i = 0; i < 6; ++i)
+        std::cout << idGenerator::getNextID();
 }
+
