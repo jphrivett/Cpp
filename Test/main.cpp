1155acbed8a771cec48d4f93c01826b9c2c275a5
Jasmine <32368030+jphrivett@users.noreply.github.com>
2018-02-16 11:25:28 +0100

    Updated filenames

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,13 +1,39 @@
-#include "testHeader1.h"
-#include "testHeader2.h"
+#include <iostream>
+#include <cmath>
 
+
+class Point2d
+{
+    double m_x;
+    double m_y;
+
+public:
+    Point2d(double x = 0.0, double y = 0.0):m_x {x}, m_y{y}
+    {
+
+    }
+
+    void print() const
+    {
+        std::cout << m_x << " " << m_y << '\n';
+    }
+
+    friend double distanceFrom(const Point2d &p1, const Point2d &p2);
+
+};
+
+    double distanceFrom(const Point2d &p1, const Point2d &p2)
+    {
+         return sqrt((p1.m_x-p2.m_x)*(p1.m_x-p2.m_x) + (p1.m_y-p2.m_y)*(p1.m_y-p2.m_y));
+    }
 
 int main()
 {
-    Storage storage(5, 6.7);
-    Display display(false);
-
-    display.displayItem(storage);
+    Point2d first;
+    Point2d second(3.0, 4.0);
+    first.print();
+    second.print();
+    std::cout << "Distance between two points: " << distanceFrom(first, second) << '\n';
 
     return 0;
 }
