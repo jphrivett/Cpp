f0547fe8e7e5c12ab117b1754bd964b9c4c9bd83
Jasmine <32368030+jphrivett@users.noreply.github.com>
2018-03-09 09:26:03 +0100

    Test

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 
 class Point
 {
@@ -43,4 +43,37 @@ int main()
     std::cout << "You entered: " << point << '\n';
 
     return 0;
+}*/
+
+#include <iostream>
+
+class Cents
+{
+private:
+    int m_cents;
+
+public:
+    Cents(int cents) { m_cents = cents; }
+
+    // Overload Cents + int
+    friend Cents operator+(const Cents &cents, int value);
+
+    int getCents() { return m_cents; }
+
+    // note: this function is not a member function!
+    Cents operator+(int value)
+    {
+        return Cents(m_cents + value);
+    }
+};
+
+
+
+int main()
+{
+	Cents cents1(6);
+	Cents cents2 = cents1 + 2;
+	std::cout << "I have " << cents2.getCents() << " cents.\n";
+
+	return 0;
 }
