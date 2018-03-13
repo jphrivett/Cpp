eacb192acdb5d5bbf9e478633d7fa60a51ed041e
Jasmine <32368030+jphrivett@users.noreply.github.com>
2018-03-13 12:25:15 +0100

    Finished

---
+++ Chap9p8QuizOverloadingSubscriptOperator/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct StudentGrade
+{
+    std::string name;
+    char grade;
+};
+
+class GradeMap
+{
+private:
+    std::vector<StudentGrade> m_map;
+
+public:
+    GradeMap()
+    {
+
+    }
+
+    char& operator[](const std::string &name);
+
+};
+
+char& GradeMap::operator[](const std::string &name)
+{
+
+    for (auto &ref: m_map)
+    {
+
+        if (ref.name == name)
+            return ref.grade;
+
+    }
+
+    StudentGrade student {name,0};
+    m_map.push_back(student);
+    return m_map.back().grade;
+
+}
+
+int main()
+{
+    GradeMap grades;
+    grades["Joe"] = 'A';
+	grades["Frank"] = 'B';
+	std::cout << "Joe has a grade of " << grades["Joe"] << '\n';
+	std::cout << "Frank has a grade of " << grades["Frank"] << '\n';
+
+    return 0;
+}
--- Test/main.cpp
+++ Test/main.cpp
@@ -1,79 +1,37 @@
-/*#include <iostream>
-
-class Point
-{
-private:
-    int m_x;
-    int m_y;
-    int m_z;
-
-public:
-    Point(int x = 0, int y = 0, int z = 0):m_x{x},m_y{y},m_z{z}
-    {
-
-    }
-
-    friend std::istream& operator>> (std::istream &in, Point &p);
-    friend std::ostream& operator<< (std::ostream &out, const Point &p);
-
-};
-
-std::istream& operator>> (std::istream &in, Point &p)
-{
-    in >> p.m_x;
-    in >> p.m_y;
-    in >> p.m_z;
-
-    return in;
-}
-
-std::ostream& operator<< (std::ostream &out, const Point &p)
-{
-    out << "(" << p.m_x << ", " << p.m_y << ", " << p.m_z << ")";
-    return out;
-}
-
-int main()
-{
-    std::cout << "Enter a point: \n";
-
-    Point point;
-    std::cin >> point;
-
-    std::cout << "You entered: " << point << '\n';
-
-    return 0;
-}*/
-
 #include <iostream>
+#include <cassert>
 
-class Cents
+class Data
 {
 private:
-    int m_cents;
+    int m_array[5] {1,2,3,4,5};
 
 public:
-    Cents(int cents) { m_cents = cents; }
-
-    // Overload Cents + int
-    friend Cents operator+(const Cents &cents, int value);
-
-    int getCents() { return m_cents; }
-
-    // note: this function is not a member function!
-    Cents operator+(int value)
+    Data()
     {
-        return Cents(m_cents + value);
+
     }
+
+    int& operator[] (const int index)
+    {
+        assert((index >= 0) && (index < 5));
+        return m_array[2];
+    }
+
+    void print()
+    {
+        for (int i:m_array)
+            std::cout << i << " ";
+
+        std::cout << '\n';
+    }
+
 };
-
-
 
 int main()
 {
-	Cents cents1(6);
-	Cents cents2 = cents1 + 2;
-	std::cout << "I have " << cents2.getCents() << " cents.\n";
+    Data data;
+    data[2] = 50;
+    data.print();
 
-	return 0;
 }
