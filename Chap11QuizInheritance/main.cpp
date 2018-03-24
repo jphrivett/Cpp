d945bafaa9e827b0d90b67213038cee1c1894f51
Jasmine <32368030+jphrivett@users.noreply.github.com>
2018-03-24 12:14:38 +0100

    Finished Qs 3a and 3b

--- Chap11QuizInheritance/main.cpp
+++ Chap11QuizInheritance/main.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+//Q2:
+/*#include <iostream>
 #include <string>
 
 class Fruit
@@ -72,3 +73,102 @@ int main()
 
 	return 0;
 }
+*/
+
+//Q3:
+#include <iostream>
+#include <string>
+
+class Creature
+{
+protected:
+    std::string m_name;
+    char m_symbol;
+    int m_health;
+    int m_damage;
+    int m_gold;
+
+public:
+    Creature(std::string name, char symbol, int health, int damage, int gold)
+        : m_name{name}, m_symbol{symbol}, m_health{health}, m_damage{damage}, m_gold{gold}
+        {
+
+        }
+
+    const std::string& getName() {return m_name;}
+    char getSymbol() {return m_symbol;}
+    int getHealth() {return m_health;}
+    int getDamage() {return m_damage;}
+    int getGold() {return m_gold;}
+
+    void reduceHealth(int health)
+    {
+        m_health -= health;
+    }
+
+    bool isDead()
+    {
+        return m_health <= 0;
+    }
+
+    void addGold(int gold)
+    {
+        m_gold += gold;
+    }
+};
+
+class Player : public Creature
+{
+private:
+    int m_level {1};
+
+public:
+    Player(std::string name) : Creature(name,'@',10,1,0)
+    {
+
+    }
+    void levelUp()
+    {
+        ++m_level;
+        ++m_damage;
+    }
+
+    int getLevel() {return m_level;}
+
+    bool hasWon() {return m_level >= 20;}
+
+};
+
+int main()
+{
+    std::cout << "Enter your name: ";
+    std::string name;
+    std::cin >> name;
+    Player p(name);
+
+    std::cout << "Welcome, " << p.getName() << ".\n";
+    std::cout << "You have " << p.getHealth()
+        << " health and are carrying " << p.getGold() << " gold.\n";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
